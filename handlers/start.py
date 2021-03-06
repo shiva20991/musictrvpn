faa9a56c72a2525742bc12a3ca3ee927700587cd
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-07 00:15:10 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,8 +4,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(
     filters.command("start")
-    & filters.group
-    & filters.private
     & ~ filters.edited
 )
 async def start(client: Client, message: Message):

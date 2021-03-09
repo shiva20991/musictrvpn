d3a0c183f7540657ba979ff5c333c3f0848a57fc
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-09 23:37:35 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -9,13 +9,8 @@ from config import DURATION_LIMIT
 from helpers.wrappers import errors
 from helpers.errors import DurationLimitError
 
-
-@Client.on_message(
-    filters.command("play")
-    & filters.group
-    & ~ filters.edited
-)
 @errors
+@Client.on_message(filters.command(["play"]))
 async def play(client: Client, message_: Message):
     audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
 

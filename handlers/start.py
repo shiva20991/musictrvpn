16faa54f89ce5a4031c6a96eaa90de8bb0259191
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-09 22:25:44 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,7 +7,7 @@ PM_START_TEXT = "Hi *{}* I'm *{}* \n I Can Play Songs On Voice chats With The Gi
 @Client.on_message(filters.command(["start"]))
 async def start(client, message):
     await message.reply_text(
-     PM_START_TEXT.format(escape_markdown(first_name),escape_markdown(bot.first_name), 
+PM_START_TEXT.format(escape_markdown(first_name),escape_markdown(bot.first_name), 
      reply_markup=InlineKeyboardMarkup(
             [
                 [

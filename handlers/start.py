53d33522a8353e72cbeba8da1590e608a6c2d002
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-09 22:28:58 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,13 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.utils.helpers import escape_markdown
+from telegram import ParseMode 
 
 PM_START_TEXT = "Hi *{}* I'm *{}* \n I Can Play Songs On Voice chats With The Given YouTube link \n But I'll Only Work On @movielinks_only \n Do /help for more Details" 
 
 @Client.on_message(filters.command(["start"]))
 async def start(client, message):
     await message.reply_text(
-PM_START_TEXT.format(escape_markdown(first_name),escape_markdown(bot.first_name), 
+     PM_START_TEXT.format(escape_markdown(first_name),escape_markdown(bot.first_name),
+     parse_mode=ParseMode.MARKDOWN, 
      reply_markup=InlineKeyboardMarkup(
             [
                 [
